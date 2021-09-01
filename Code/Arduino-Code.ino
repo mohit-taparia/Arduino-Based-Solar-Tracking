dbf4a2aa23e54b5da5ee8e66abc92b1b505bbb8b
Mohit Rakesh Taparia <67408189+mohit-taparia@users.noreply.github.com>
2021-09-01 08:24:14 +0530

    Update Arduino-Code.ino

--- Code/Arduino-Code.ino
+++ Code/Arduino-Code.ino
@@ -5,16 +5,6 @@
   optimize solar power generation with 40%-60% improvement in efficiency over fixed PV 
   panel installations.
   The circuit:
-   
-  CODE REVISIONS:
-  1. Initial code - modified 21 May 2017 By Ryan Downer
-  
-  LICENSING:
-    GNU General Public License v3.0
-    Permissions of this strong copyleft license are conditioned on making available
-    complete source code of licensed works and modifications, which include larger 
-    works using a licensed work, under the same license. Copyright and license notices
-    must be preserved. Contributors provide an express grant of patent rights.
 */
 
 
